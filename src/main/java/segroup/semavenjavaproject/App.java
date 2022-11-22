fabc024dc284886208ae70781ba4fe2679bbf5db
madhuri <7036156189madhu@gmail.com>
2022-11-22 12:56:49 +0530

    first commit to Maven Java Project

---
+++ src/main/java/segroup/semavenjavaproject/App.java
@@ -0,0 +1,13 @@
+package segroup.semavenjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from madhuri 6692" );
+    }
+}
